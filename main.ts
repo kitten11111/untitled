18134748baaebbf8ae76878f26237b13568f57b6
kitten11111 <163400849+kitten11111@users.noreply.github.com>
2024-07-22 15:21:40 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,39 @@
 basic.forever(function () {
-    JSON
+    for (let index = 0; index < 4; index++) {
+        for (let index = 0; index < 4; index++) {
+            JSON
+continue;
+basic.showLeds(`
+                # . . . #
+                . . . . .
+                . . # . .
+                . . . . .
+                # . . . #
+                `)
+            break;
+basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . # . .
+                . # . # .
+                . . . . .
+                `)
+            basic.showLeds(`
+                # . . . #
+                . . . . .
+                . . # . .
+                . . . . .
+                # . . . #
+                `)
+            continue;
+basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . # . .
+                . # . # .
+                . . . . .
+                `)
+            break;
+        }
+    }
 })
-keyof
